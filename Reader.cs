cca6d7d3798bfbf4f0a50140f283d5840b3d0145
Ryan Kojan <rkojan@gmail.com>
2017-04-06 20:11:22 -0700

    Added new way to import CSV to objects

--- Reader.cs
+++ Reader.cs
@@ -14,43 +14,12 @@ namespace Homework_1
 		}
 
 		public void ReadInFile() {
-			Sale temp = new Sale();
+			List<Sale> values = File.ReadAllLines("")
+									.Skip(1)
+			                        .Select(x => Sale.FromCsv(x))
+									.ToList();
 
-			using (var filePath = File.OpenRead(@"/Users/ryankojan/Desktop/Spring2017/cs340/cs162hw01salesData01.csv"))
-			using (var reader = new StreamReader(filePath))
-			{
-				List<DateTime> TransactionDates = new List<DateTime>();
-				List<string> Products = new List<string>();
-				List<int> Prices = new List<int>();
-				List<string> Cities = new List<string>();
-				List<string> States = new List<string>();
-				List<string> Countries = new List<string>();
-				List<string> Names = new List<string>();
-				List<DateTime> AccountCreated = new List<DateTime>();
-				List<DateTime> LastLogin = new List<DateTime>();
-				List<float> Latitudes = new List<float>();
-				List<float> Longitudes = new List<float>();
-				while (!reader.EndOfStream)
-				{
-					var line = reader.ReadLine();
-					var values = line.Split(',');
-
-					TransactionDates.Add(Convert.ToDateTime(values[0]));
-					Products.Add(values[1]);
-					Prices.Add(Convert.ToInt32(values[2]));
-					Cities.Add(values[3]);
-					States.Add(values[4]);
-					Countries.Add(values[5]);
-					Names.Add(values[6]);
-					AccountCreated.Add(Convert.ToDateTime(values[7]));
-					LastLogin.Add(Convert.ToDateTime(values[8]));
-					Latitudes.Add(Convert.ToSingle(values[9]));
-					Longitudes.Add(Convert.ToSingle(values[10]));
-
-					Console.WriteLine(values[1]);
-				}
-			}
-
+			_allSales = values;
 		}
 
 		public int AvaragePurchaseAmount() {
--- Sale.cs
+++ Sale.cs
@@ -15,5 +15,25 @@ namespace Homework_1
 		public DateTime LastLogin { get; set; }
 		public float Latitude { get; set; }
 		public float Longitude { get; set; }
+
+		public static Sale FromCsv(String CsvLine)
+		{
+			string[] values = CsvLine.Split(',');
+			Sale temp = new Sale();
+			temp.TransactionDate = Convert.ToDateTime(values[0]);
+			temp.Product = Convert.ToString(values[1]);
+			temp.Price = Convert.ToInt32(values[2]);
+			temp.PaymentType = Convert.ToString(values[3]);
+			temp.City = Convert.ToString(values[4]);
+			temp.State = Convert.ToString(values[5]);
+			temp.Country = Convert.ToString(values[6]);
+			temp.Name = Convert.ToString(values[7]);
+			temp.AccountCreated = Convert.ToDateTime(values[8]);
+			temp.LastLogin = Convert.ToDateTime(values[9]);
+			temp.Latitude = Convert.ToSingle(values[10]);
+			temp.Longitude = Convert.ToSingle(values[11]);                                      
+
+			return temp;
+		}
 	}
 }
