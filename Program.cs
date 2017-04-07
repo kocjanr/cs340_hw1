6bb4183469e954a21185952bba95956f8384ee5e
kocjanr <kocjanr@onid.oregonstate.edu>
2017-04-06 17:11:57 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Reader read = new Reader();
+		}
+	}
+}
---
+++ Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Homework_1
+{
+	public class Sale
+	{
+		public DateTime TransactionDate { get; set;}
+		public string Product { get; set; }
+		public int Price { get; set;}
+		public string PaymentType { get; set;}
+		public string City { get; set;}
+		public string State { get; set;}
+		public string Country { get; set; }
+		public string Name { get; set; }
+		public DateTime AccountCreated { get; set; }
+		public DateTime LastLogin { get; set; }
+		public float Latitude { get; set; }
+		public float Longitude { get; set; }
+	}
+}
